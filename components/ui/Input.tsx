f4847d2b5eaff13978b2e57700044c4281dd3ae4
yash110-prog <janiyash0911@gmail.com>
2026-02-14 17:47:52 +0530

    fix hydration warning

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,3 +1,16 @@
-export default function Input({ placeholder }) {
-  return <input className="border p-2" placeholder={placeholder} />;
+type Props = {
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Input({ placeholder, value, onChange }: Props) {
+  return (
+    <input
+      className="border p-2 rounded w-full"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
